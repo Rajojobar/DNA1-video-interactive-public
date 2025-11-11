4165678e8658789db193fef5ac6aaf80f20059b0
raphaël <r.bar772@laposte.net>
2025-11-11 18:32:11 +0100

    ajout de nouvelles séquences, suppression d'un matériel cpyrighté, ajout d'un nouveau matériel copyrighté, et le main peut jouer du son

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from player_video import PlayerVideo
 from player_opengl import PlayerOpenGl
 from player_image import PlayerImage
 from compteur import Compteur
+from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PyQt6.QtCore import QUrl
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -27,28 +29,59 @@ if __name__ == "__main__":
         inst.showFullScreen()
         return inst
     
+    def play_audio(file_path, volume=50):
+        audio_output = QAudioOutput()
+        audio_output.setVolume(volume / 100)  # Volume between 0.0 and 1.0
+
+        # Ensure the audio output is not muted
+        audio_output.setMuted(False)
+
+        media_player = QMediaPlayer()
+        media_player.setAudioOutput(audio_output)
+        media_player.setSource(QUrl.fromLocalFile(file_path))
+        
+        # Keep a reference to prevent garbage collection
+        windows.append(media_player)
+        windows.append(audio_output)
+        
+        media_player.play()
+        return media_player
+
     # Generated
     def schedule(callable_obj, delay_ms):
         QTimer.singleShot(delay_ms, callable_obj)
 
-    # Début du film (titre)
-    schedule(partial(show_instance_fullscreen, PlayerImage, "src/titre.png", size=(1920,1080), titre="Titre", death=20000), 0)
-    
-    schedule(partial(show_instance, Compteur, death=999999, isFunny=True), 11000)
-    schedule(partial(show_instance, PlayerVideo, "src/rush_1.mp4", isFunny=True, mute=True), 15000)
 
+
+    # Début du film (titre) Durée : 30 secondes
+    schedule(partial(show_instance_fullscreen, PlayerImage, "src/titre.png", size=(1920,1080), titre="Titre", death=30000), 0)
+    
+    # Début de mla bande son à 8 secondes
+    schedule(partial(play_audio, "src/audio1.mp3", volume=70), 8000)
+
+    # Création du compteur à 8 secondes 
+    schedule(partial(show_instance, Compteur, death=999999, isFunny=True), 8000)
+    
+    # OpenGL Clifford Attractor 1 à 20 secondes Termine à 40 secondes
     from opengl_clifford_attractor import CliffordAttractorShader as Clifford0
     schedule(
         partial(
             show_instance,
             PlayerOpenGl,
             Clifford0,
-            titre="Clifford Attractor 1",
+            titre="Chaos Attractor 1",
             position=(800, 500),
             death=200000,
             isFunny=True,
         ),
-        12000,
+        20000,
     )
+
+    # Vidéo rush 1 à 29 secondes
+    schedule(partial(show_instance, PlayerVideo, "src/rush_1.mp4", isFunny=True, mute=True), 24000)
+
+    # Vidéo rush 2 à 29 secondes
+    schedule(partial(show_instance, PlayerVideo, "src/rush_2.mp4", size=(900,1440), isFunny=True, mute=False), 30000)
+
 
     sys.exit(app.exec())
