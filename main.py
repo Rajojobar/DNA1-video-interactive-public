1cb511de1084cb6d504c948ac073d672415f223c
raphaël <r.bar772@laposte.net>
2025-11-11 15:05:55 +0100

    ça marche plus ???

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from player_video import PlayerVideo
-from player_opengl import PlayerOpenGl
 
-from opengl_random_color import RandomColorShader
-from opengl_clifford_attractor import CliffordAttractorShader
+# from opengl_random_color import RandomColorShader
 
-# TODO : Securiser les fenêtres individuelles pour gestion de crash 
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
     # Exemple : lancer plusieurs vidéos en même temps
-    # player1 = PlayerVideo("src/video.mp4", mute=True)
-    # player2 = PlayerVideo("src/video.mp4", mute=True)
-    # player1.show()
-    # player2.show()
+    player1 = PlayerVideo("src/video.mp4", mute=True)
+    player2 = PlayerVideo("src/video.mp4", mute=True)
+    player1.show()
+    player2.show()
 
     # player3 = PlayerOpenGl(RandomColorShader, titre="OpenGL Random Color", death=5000)
     # player3.show()
 
-
-    player4 = PlayerOpenGl(CliffordAttractorShader, titre="OpenGL Random Color", death=1000000)
-    player4.show()
+    # from opengl_clifford_attractor import CliffordAttractorShader as Clifford0
+    # player4 = PlayerOpenGl(Clifford0, titre="Clifford Attractor 1", death=200000)
+    # player4.show()
 
 
     # player4 = rajojoQtWidget(titre="Rajojo Qt Widget", death=15000)
