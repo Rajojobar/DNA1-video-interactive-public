b7a78e24d69f80ef689ddb97c42f9b47cbe8165b
raphaël <r.bar772@laposte.net>
2025-11-12 09:18:28 +0100

    Jeu de la vie de conway est opérationnel

--- opengl_conway.py
+++ opengl_conway.py
@@ -2,9 +2,8 @@ import sys
 from PyQt6 import QtOpenGL
 from OpenGL import GL
 import numpy as np
-from random import randint
+from random import randint, choice
 from PyQt6.QtWidgets import QApplication
-
 
 # def draw_points():
 #     glBegin(GL_POINTS)
@@ -25,36 +24,47 @@ from PyQt6.QtWidgets import QApplication
 class ConwaySimulation(QtOpenGL.QOpenGLWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.resize(300, 300)
-        self._width = 300
-        self._height = 300
+        self.resize(290, 250)
+        # self._width = 300
+        # self._height = 300
 
+        self.game_width = 100
+        self.game_height = 100
+
+        self.strongColor = choice([(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (0, 1, 1), (1, 0, 1)])
         self.matrice = np.zeros((300, 300), dtype=int)
-
-        self.scale = 89.0  # scale to make the attractor visible in 300x300
-
+        self.puissanceCouleur = randint(70,150)
+        
+        self.countIterations = 0
         self.reset()
-    
+        
     def reset(self):
-        self.matrice = np.zeros((300, 300), dtype=int)
-        self.matrice[149, 299] = 1
-        self.matrice[151, 299] = 1
-        self.matrice[150, 299] = 1
+        self.matrice = np.zeros((self.game_width, self.game_height), dtype=int)
+        # self.matrice[149, 299] = 1
+        # self.matrice[151, 299] = 1
+        # self.matrice[150, 298] = 1
+        self.strongColor = choice([(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (0, 1, 1), (1, 0, 1)])
+        self.puissanceCouleur = randint(70,200)
+
+        for _ in range(self.game_width * self.game_height // 10):
+            self.matrice[randint(0, self.game_width-1), randint(0, self.game_height-1)] = 1
 
     def calculate_next_color(self, color):
+        base = randint(0, 255)
+        
         # slightly fade the color so older points appear darker
-        r = max(0, int(color[0]) - 1)
-        g = max(0, int(color[1]) - 2)
-        b = max(0, int(color[2]) - 1)
+        r = max(0, int(base) - self.strongColor[0]*self.puissanceCouleur)
+        g = max(0, int(base) - self.strongColor[1]*self.puissanceCouleur)
+        b = max(0, int(base) - self.strongColor[2]*self.puissanceCouleur)
         # return (255, 255, 255)
         return (r, g, b)
     
     def calculate_next_state(self, x, y):
         # Count live neighbors
         total = (
-            self.matrice[(x-1) % 300, (y-1) % 300] + self.matrice[(x-1) % 300, y % 300] + self.matrice[(x-1) % 300, (y+1) % 300] +
-            self.matrice[x % 300, (y-1) % 300] +                                     self.matrice[x % 300, (y+1) % 300] +
-            self.matrice[(x+1) % 300, (y-1) % 300] + self.matrice[(x+1) % 300, y % 300] + self.matrice[(x+1) % 300, (y+1) % 300]
+            self.matrice[(x-1) % self.game_width, (y-1) % self.game_height] + self.matrice[(x-1) % self.game_width, y % self.game_height] + self.matrice[(x-1) % self.game_width, (y+1) % self.game_height] +
+            self.matrice[x % self.game_width, (y-1) % self.game_height] + self.matrice[x % self.game_width, (y+1) % self.game_height] +
+            self.matrice[(x+1) % self.game_width, (y-1) % self.game_height] + self.matrice[(x+1) % self.game_width, y % self.game_height] + self.matrice[(x+1) % self.game_width, (y+1) % self.game_height]
         )
         if self.matrice[x, y] == 1:
             if total < 2 or total > 3:
@@ -84,7 +94,7 @@ class ConwaySimulation(QtOpenGL.QOpenGLWindow):
 
     def paintGL(self):
 
-        GL.glPointSize(randint(1, 5))
+        GL.glPointSize(randint(2,6))
 
         GL.glClear(GL.GL_COLOR_BUFFER_BIT)
         # draw all points between glBegin/glEnd so OpenGL actually renders them
@@ -95,13 +105,16 @@ class ConwaySimulation(QtOpenGL.QOpenGLWindow):
         
         nouvelle_matrice = np.copy(self.matrice)
         
-        for i in range(len(self.matrice)-1):
-            for j in range(len(self.matrice)-1):
+        for i in range(self.game_width-1):
+            for j in range(self.game_height-1):
 
                 if self.matrice[i, j] == 1:
                     couleur = self.calculate_next_color(couleur)
                     GL.glColor3ub(couleur[0], couleur[1], couleur[2])
-                    GL.glVertex2f(i, j)
+                    upscaled_x = int(i * (self._width / self.game_width))
+                    upscaled_y = int(j * (self._height / self.game_height))
+                    GL.glVertex2f(upscaled_x, upscaled_y)
+                    #GL.glVertex2f(i, j)
 
                 nouvelle_valeur = self.calculate_next_state(i, j)
                 nouvelle_matrice[i, j] = nouvelle_valeur
@@ -109,7 +122,14 @@ class ConwaySimulation(QtOpenGL.QOpenGLWindow):
         GL.glEnd()
         self.matrice = nouvelle_matrice
         # Request a new frame to be rendered
+  
         self.update()
+
+        self.countIterations += 1
+
+        if self.countIterations >= 70:
+            self.countIterations = 0
+            self.reset()
 
 
 if __name__ == "__main__":
@@ -117,7 +137,7 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
-    player = PlayerOpenGl(ConwaySimulation)
+    player = PlayerOpenGl(ConwaySimulation, titre="Conway")
 
     player.show()
     sys.exit(app.exec())
